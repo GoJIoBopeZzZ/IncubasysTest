77e88799d39a7ca17ffc835cd645c5db70e941d7
GoJIoBopeZzZ <apple.tyomka@gmail.com>
2018-08-15 20:50:18 +0300

    Рефакторинг CircleCI v2.1 + Fabric

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,9 @@
+#!/usr/bin/env bash
 #/bin/bash
 
 cd ..
 GRADLE="./gradlew"
 $GRADLE assembleRelease
 $GRADLE crashlyticsUploadDistributionRelease
-RELEASEFILE="${CIRCLE_ARTIFACTS}/habrcicdcat-release-${CIRCLE_BUILD_NUM}-${CIRCLE_SHA1}.apk"
+RELEASEFILE="${CIRCLE_ARTIFACTS}/incubasystest-release-${CIRCLE_BUILD_NUM}-${CIRCLE_SHA1}.apk"
 cp ./app/build/outputs/apk/app-release.apk ${RELEASEFILE}
